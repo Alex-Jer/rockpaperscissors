8914f0fbdac788a32b78b78c5c69571f3db6a2ee
Alex <alexandrejeronimo2001@gmail.com>
2020-07-21 00:43:57 +0100

    update

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    'implicit-arrow-linebreak': 'off',
+    'no-console': 'off',
+  },
+};
